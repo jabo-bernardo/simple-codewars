6e9d4ad79bbe31217d52ecd21f1b8565c27f2a90
Jabo <joelvincent.work@gmail.com>
2020-08-21 10:59:13 +0800

    🔧 Basic Layout & Added Content

--- js/app.js
+++ js/app.js
@@ -2,32 +2,6 @@ let editor = CodeMirror.fromTextArea(document.querySelector("textarea"), {
     lineNumbers: true,
     mode: "javascript",
     theme: "material-darker"
-})
+});
 
-let willRunCode = setTimeout(() => runCode(), 1000)
-
-editor.on("change", () => {
-    clearInterval(willRunCode);
-})
-
-editor.on("changes", () => {
-    willRunCode = setTimeout(() => runCode(), 1000);
-})
-
-function runCode() {
-    let output = null
-    $(".out").classList.remove("red");
-    try {
-        output = eval(editor.getValue());
-        if(output == undefined) return $(".out").innerHTML = "...";
-        $(".out").innerHTML = output;
-    } catch(err) {
-        output = err.toString();
-        $(".out").classList.add("red");
-        $(".out").innerHTML = output;
-    }
-}
-
-function $(s) {
-    return document.querySelector(s);
-}
+editor.setValue(`function isDigit(str) {\n\treturn false;\n}`)
